7d0972f0986d9211b3755a7a3c7d8b2ef8e6d5de
souis <ons.souissi01@gmail.com>
2025-04-30 19:51:56 +0100

    envoie de mail de confirmation

--- app/src/main/java/com/example/spectacleapp/service/ApiClient.java
+++ app/src/main/java/com/example/spectacleapp/service/ApiClient.java
@@ -8,7 +8,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 
 public class ApiClient {
-    private static final String BASE_URL = "http://192.168.94.165:8081/";
+    private static final String BASE_URL = "http://192.168.253.165:8081/";
     private static Retrofit retrofit = null;
 
     public static SpectacleService getSpectacleService() {
